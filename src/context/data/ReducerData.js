7ea9fa995bfa0cad74a4e12d1603bdd9ea1a4c97
mirolyubovaalyona <mirolyubovaalyona@gmail.com>
2024-06-14 01:28:48 +1000

    News back emulation
    + Запросы на сервер по роуту '/news' , отрисовка новостей

--- src/context/data/ReducerData.js
+++ src/context/data/ReducerData.js
@@ -1,6 +1,11 @@
 const ReducerData = (state, action) => {
     switch (action.type) {
-     
+        case "FETCH_NEWS":
+            console.log(action)
+            return{// '...' - проходимся оператором чтобы стейт не мутировал
+                ...state,
+                news: action.payload
+            }
         default:
             return state;
     }
--- src/routes/News/News.js
+++ src/routes/News/News.js
@@ -2,13 +2,45 @@ import React from "react";
 import Navbar from './../../modules/navigation/Navbar';
 import classes from './../../style.module.scss';
 import Header from "../../modules/navigation/Header";
+import ContextData from './../../context/data/ContextData';
 
 const News = () => {
+    const {stateData, dispatchData} = React.useContext(ContextData)
+    const news = stateData.news
+    React.useEffect(() => {
+        const fetchNews = async() => {
+            try {
+                const response = await fetch('http://127.0.0.1:3004/news')
+                if (response.status === 200 ) {
+                    const result =  await response.json() // работает только если дожидаться ответ
+                    dispatchData({
+                        type: "FETCH_NEWS",
+                        payload: result
+                    })
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        };
+        fetchNews()
+    },[])
+
     return(
         <div>
             <div className={classes.main}>
                 <div className={classes.main_Navigation}><Navbar /></div>
-                <div className={classes.main_Content}><Header /></div>
+                <div className={classes.main_Content}>
+                    <Header />
+                    { news.map((elem, index) => {
+                        return (
+                            <div key={index}>
+                                <div>{elem.title}</div>
+                                <div>{elem.description}</div>
+                                <div><img src = {elem.image}/></div>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>           
         </div>
     )
