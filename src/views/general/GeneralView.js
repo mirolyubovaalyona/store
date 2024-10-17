3c816159b2d9397a4def13813eccc721dbaf6120
mirolyubovaalyona <mirolyubovaalyona@gmail.com>
2024-10-18 03:46:30 +1000

    шаблон авторизации

--- src/views/general/GeneralView.js
+++ src/views/general/GeneralView.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from './../../styles/views/general-styles.module.scss'
+import Input from "../../components/ui/Input/Input";
+import Button from "../../components/ui/Button/Button";
 
 const GeneralView = () => {
+    const [value, setValue] = useState({})
 
+    const sendValue = () => {
+        console.log('sending')
+    }
     return (
         <div className={classes.wrapper}>
             <div>
                 <div>Вход в систему</div>
-                <div>imputs</div>
-                <div>Botton</div>
+                <div>
+                    <Input
+                        value={value}
+                        setValue={setValue}
+                        name = 'login'
+                        type = 'text'
+                    />
+                    <Input
+                        value={value}
+                        setValue={setValue}
+                        name = 'password'
+                        type = 'password'
+                    />
+                </div>
+                <div>
+                    <Button
+                        text='Вход'
+                        onClick = {sendValue}
+                    />
+                </div>
             </div>
         </div>
     )
